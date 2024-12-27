6e0be6cb0d3412c5844eb9b408fc34fd622b295e
Keito Tobita <tbtkit0916@gmail.com>
2024-12-27 20:48:25 +0900

    app5.jsとREADMEを編集

--- app7.js
+++ app7.js
@@ -1,8 +1,12 @@
+"use strict";
 const express = require("express");
 const app = express();
 
+let bbs = [];  // 本来はDBMSを使用するが，今回はこの変数にデータを蓄える
+
 app.set('view engine', 'ejs');
 app.use("/public", express.static(__dirname + "/public"));
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/hello1", (req, res) => {
   const message1 = "Hello world";
@@ -21,32 +25,16 @@ app.get("/icon", (req, res) => {
 app.get("/luck", (req, res) => {
   const num = Math.floor( Math.random() * 6 + 1 );
   let luck = '';
-  if(num == 1)luck = "大吉"
-  else if(num == 2)luck = "吉"
-  else if(num == 3)luck = "中吉"
-  else if(num == 4)luck = "小吉"
-  else if(num == 5)luck = "末吉"
-  else if(num == 6)luck = "凶"
-
+  if( num==1 ) luck = '大吉';
+  else if( num==2 ) luck = '中吉';
+  console.log( 'あなたの運勢は' + luck + 'です' );
   res.render( 'luck', {number:num, luck:luck} );
 });
-app.get("/dice", (req, res) => {
-  const num = Math.floor( Math.random() * 100 + 1 );
-  let luck = num;
-  res.render( 'dice', {number:num, luck:luck} );
-});
-app.get("/coin", (req, res) => {
-  const num = Math.floor( Math.random() * 2 + 1 );
-  let luck = '';
-  if(num == 1)luck = "表"
-  else if(num == 2)luck = "裏"
 
-  res.render( 'coin', {number:num, luck:luck} );
-});
 app.get("/janken", (req, res) => {
   let hand = req.query.hand;
-  let win = Number(req.query.win) || 0;
-  let total = Number(req.query.total) || 0;
+  let win = Number( req.query.win );
+  let total = Number( req.query.total );
   console.log( {hand, win, total});
   const num = Math.floor( Math.random() * 3 + 1 );
   let cpu = '';
@@ -54,22 +42,9 @@ app.get("/janken", (req, res) => {
   else if( num==2 ) cpu = 'チョキ';
   else cpu = 'パー';
   // ここに勝敗の判定を入れる
-  let judgement = '';
-  if (hand === cpu) {
-    judgement = '引き分け';
-  } 
-  else if (
-    (hand === 'グー' && cpu === 'チョキ') ||
-    (hand === 'チョキ' && cpu === 'パー') ||
-    (hand === 'パー' && cpu === 'グー')
-  ) {
-    judgement = '勝ち';
-    win += 1;
-  } 
-  else {
-    judgement = '負け';
-  }
   // 今はダミーで人間の勝ちにしておく
+  let judgement = '勝ち';
+  win += 1;
   total += 1;
   const display = {
     your: hand,
@@ -80,11 +55,23 @@ app.get("/janken", (req, res) => {
   }
   res.render( 'janken', display );
 });
+
 app.get("/get_test", (req, res) => {
   res.json({
     answer: 0
   })
 });
+
+app.get("/add", (req, res) => {
+  console.log("GET");
+  console.log( req.query );
+  const num1 = Number( req.query.num1 );
+  const num2 = Number( req.query.num2 );
+  console.log( num1 );
+  console.log( num2 );
+  res.json( {answer: num1+num2} );
+});
+
 app.post("/add", (req, res) => {
   console.log("POST");
   console.log( req.body );
@@ -94,4 +81,28 @@ app.post("/add", (req, res) => {
   console.log( num2 );
   res.json( {answer: num1+num2} );
 });
+
+// これより下はBBS関係
+app.post("/check", (req, res) => {
+  // 本来はここでDBMSに問い合わせる
+  res.json( {number: bbs.length });
+});
+
+app.post("/read", (req, res) => {
+  // 本来はここでDBMSに問い合わせる
+  const start = Number( req.body.start );
+  console.log( "read -> " + start );
+  if( start==0 ) res.json( {messages: bbs });
+  else res.json( {messages: bbs.slice( start )});
+});
+
+app.post("/post", (req, res) => {
+  const name = req.body.name;
+  const message = req.body.message;
+  console.log( [name, message] );
+  // 本来はここでDBMSに保存する
+  bbs.push( { name: name, message: message } );
+  res.json( {number: bbs.length } );
+});
+
 app.listen(8080, () => console.log("Example app listening on port 8080!"));
